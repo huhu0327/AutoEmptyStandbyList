ed6673d60103a9d8787e6bfcf315013cdb45de14
Dareum Nam <namdalen1115@gmail.com>
2024-02-05 05:32:37 +0900

    Preference 파일 없을 때, 초기 시간 적용 안되는 문제 해결

--- AutoEmptyStandbyList.Client/ViewModels/MainWindowViewModel.cs
+++ AutoEmptyStandbyList.Client/ViewModels/MainWindowViewModel.cs
@@ -46,15 +46,13 @@ partial class MainWindowViewModel : ViewModelBase
     private void ApplyPreferences(int defaultTimerMinute)
     {
         _preferences = _preferencesesService.LoadPreferences();
-        if (_preferences is not null)
-        {
-            TimerMinute = _preferences.TimerMinute;
-        }
-        else
+        if (_preferences is null)
         {
             _preferences = new() { TimerMinute = defaultTimerMinute, TaskCreated = false };
             _preferencesesService.SavePreferences(_preferences);
         }
+
+        TimerMinute = _preferences.TimerMinute;
 
         if (!_preferences.TaskCreated)
         {
